8fb7db5a005a55db40857e33dc4f287dfdf719b4
Tamanit <78948032+Tamanit@users.noreply.github.com>
2023-04-14 10:28:40 +0500

    tring to make walking animation

--- MainCharacter/BasicActions.cs
+++ MainCharacter/BasicActions.cs
@@ -18,7 +18,7 @@ public class BasicActions : MonoBehaviour
         sprite.GetComponent<SpriteRenderer>();
         animator.GetComponent<Animator>();
     }
-    
+
     void FixedUpdate()
     {
         //if (body.isGrounded){}
@@ -27,9 +27,9 @@ public class BasicActions : MonoBehaviour
         _movingVector = transform.TransformDirection(_movingVector);
         _movingVector *= movementSpeed;
 
-        body.Move(_movingVector * Time.deltaTime); 
-    
-    
+        body.Move(_movingVector * Time.deltaTime);
+
+
         /*body.velocity = Input.GetKey(KeyCode.W) ? Vector2.up * movementSpeed
                   : Input.GetKey(KeyCode.A) ? Vector2.left * movementSpeed
                   : Input.GetKey(KeyCode.S) ? Vector2.down * movementSpeed
@@ -40,23 +40,24 @@ public class BasicActions : MonoBehaviour
         _movingVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         _movingVector *= movementSpeed;
         body.velocity = _movingVector;
-       */ 
+       */
         switch (body.velocity.x)
         {
-            case >0:
+            case > 0:
                 sprite.flipX = true;
                 break;
-            case <0:
+            case < 0:
                 sprite.flipX = false;
                 break;
         }
-        
+
         //var isUpDownMoving = Input.GetButtonDown(KeyCode.)
         //var ismoving = body.velocity.x != 0
-        
+
         //animator.SetFloat("UpDownAxis", Input.GetAxis("Vertical"));
-        animator.SetBool("XMove",  Math.Abs(body.velocity.x) > 0);
-        
+        animator.SetBool("XMove", Math.Abs(body.velocity.x) > 0);
+        animator.SetFloat("YMove", Input.GetAxis("Vertical"));
+
         //if (body.velocity.x > 0) sprite.flipX = true;
         //if (body.velocity.x < 0) sprite.flipX = false;
 
