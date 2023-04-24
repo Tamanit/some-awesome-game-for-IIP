7d905f7b81f097bebe47594d14b148851811216c
literrarian <razdolbaika1@gmail.com>
2023-04-24 14:08:03 +0500

    urp
    crow is lit now!!! yay!!

--- MainCharacter/CameraControl.cs
+++ MainCharacter/CameraControl.cs
@@ -1,35 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
     public Camera camera;
+
     public float zoomStep;
-    public float maxZoomSize;
+    public float maxZoomSize = 121;
+    public float minZoomSize = 51;
     public float upToCenterSize;
-    public float minZoomSize;
-    // Start is called before the first frame update
-    void Start()
-    {
-        camera.GetComponent<Camera>();
-    }
-    
+
+
     void FixedUpdate()
     {
-        var transformPosition = new Vector3();
-        transformPosition.z = -10;
-        transformPosition.x = gameObject.transform.position.x;
-        transformPosition.y = gameObject.transform.position.y + upToCenterSize;
-        camera.transform.position = transformPosition;
-        
+        var playerPosition = gameObject.transform.position;
+
+        camera.transform.position = new Vector3
+        (
+            playerPosition.x,
+            playerPosition.y + upToCenterSize,
+            -10
+
+        );
+
         switch (Input.GetAxis("Mouse ScrollWheel"))
         {
+
             case (float)0.1:
-                camera.orthographicSize += camera.orthographicSize == maxZoomSize ? 0: zoomStep;
+                camera.fieldOfView += camera.fieldOfView >= maxZoomSize ? 0 : zoomStep;
                 break;
             case (float)-0.1:
-                camera.orthographicSize -= camera.orthographicSize == minZoomSize ? 0: zoomStep;
+                camera.fieldOfView -= camera.fieldOfView <= minZoomSize ? 0 : zoomStep;
                 break;
         }
 
