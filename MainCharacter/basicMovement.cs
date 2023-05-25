01e83524a08ff63da072814e6caf4613fcdd39ec
literrarian <razdolbaika1@gmail.com>
2023-05-25 08:40:40 +0500

    a

--- MainCharacter/basicMovement.cs
+++ MainCharacter/basicMovement.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class BasicActions : MonoBehaviour
+public class basicMovement : MonoBehaviour
 {
     public float movementSpeed = 1000f;
     public Rigidbody2D body;
     public SpriteRenderer sprite;
     public Animator animator;
     private Vector3 _movingVector;
-    
+
     void Start()
     {
         body.GetComponent<Rigidbody2D>();
@@ -21,8 +21,8 @@ public class BasicActions : MonoBehaviour
 
     void FixedUpdate()
     {
-        
-        _movingVector = new Vector2(Input.GetAxis("Horizontal")*movementSpeed,0);
+
+        _movingVector = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, 0);
         body.velocity = _movingVector;
 
         switch (body.velocity.x)
