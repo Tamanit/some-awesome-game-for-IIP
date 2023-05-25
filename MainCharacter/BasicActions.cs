afbfa12ce92f0385b95ff12fb1831022da0545b9
Tamanit <a.plokhikh.sas@gmail.com>
2023-05-25 08:13:15 +0500

    r

--- MainCharacter/BasicActions.cs
+++ MainCharacter/BasicActions.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 public class BasicActions : MonoBehaviour
 {
     public float movementSpeed = 1000f;
-    //public CharacterController body;
     public Rigidbody2D body;
     public SpriteRenderer sprite;
     public Animator animator;
@@ -22,24 +21,8 @@ public class BasicActions : MonoBehaviour
 
     void FixedUpdate()
     {
-        //if (body.isGrounded){}
-
-        //_movingVector = new Vector3(Input.GetAxis("Horizontal"), -5, 0);
-        //_movingVector = transform.TransformDirection(_movingVector);
-        //_movingVector *= movementSpeed;
-
-        //body.Move(_movingVector * Time.deltaTime);
-
-
-        /*body.velocity = Input.GetKey(KeyCode.W) ? Vector2.up * movementSpeed
-                  : Input.GetKey(KeyCode.A) ? Vector2.left * movementSpeed
-                  : Input.GetKey(KeyCode.S) ? Vector2.down * movementSpeed
-                  : Input.GetKey(KeyCode.D) ? Vector2.right * movementSpeed
-        : Vector2.zero;
-*/
-
+        
         _movingVector = new Vector2(Input.GetAxis("Horizontal")*movementSpeed,0);
-       // _movingVector *= movementSpeed;
         body.velocity = _movingVector;
 
         switch (body.velocity.x)
@@ -54,7 +37,5 @@ public class BasicActions : MonoBehaviour
 
         animator.SetBool("XMove", Math.Abs(body.velocity.x) > 0);
         animator.SetFloat("YMove", Input.GetAxis("Vertical"));
-        
-        
     }
 }
